a768aed7e5e5f3ade0577edc5dc7615c4825f5a7
Wameuh <72522288+Wameuh@users.noreply.github.com>
2021-05-13 12:58:14 +0200

    Création du fichier configuration

---
+++ configuration.h
@@ -0,0 +1,8 @@
+#define SSID "yourSSID"
+#define WIFI_PASS "yourwifipassword"
+#define HOST "FTP_adress"
+#define PORT 21
+#define USERNAME "FTP_username"
+#define FTP_PASSWORD "FTP_password"
+#define FILENAME "name_of_the_file" // nom du fichier à modifier
+#define DIRNAME "folder_on_FTP" // répertoire du fichier dans le FTP
