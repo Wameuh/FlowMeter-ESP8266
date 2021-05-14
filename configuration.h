3a1a0019c64cda73bb58d3e3d7dae6ae9ec0d712
Wameuh <72522288+Wameuh@users.noreply.github.com>
2021-05-14 15:31:51 +0200

    Changement nom variables

--- configuration.h
+++ configuration.h
@@ -1,8 +1,8 @@
-#define SSID "yourSSID"
+#define WIFI_SSID "yourSSID"
 #define WIFI_PASS "yourwifipassword"
-#define HOST "FTP_adress"
-#define PORT 21
-#define USERNAME "FTP_username"
+#define FTP_HOST "FTP_adress"
+#define FTP_PORT 21
+#define FTP_USERNAME "FTP_username"
 #define FTP_PASSWORD "FTP_password"
 #define FILENAME "name_of_the_file" // nom du fichier à modifier
-#define DIRNAME "folder_on_FTP" // répertoire du fichier dans le FTP
+#define FTP_DIRNAME "folder_on_FTP" // répertoire du fichier dans le FTP
