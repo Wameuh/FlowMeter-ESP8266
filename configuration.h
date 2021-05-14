add677daa83663fc5588f4066713726ab0a5d929
Wameuh <72522288+Wameuh@users.noreply.github.com>
2021-05-14 17:00:13 +0200

    Update configuration.h

--- configuration.h
+++ configuration.h
@@ -4,5 +4,5 @@
 #define FTP_PORT 21
 #define FTP_USERNAME "FTP_username"
 #define FTP_PASSWORD "FTP_password"
-#define FILENAME "name_of_the_file" // nom du fichier à modifier
+#define FTP_FILENAME "name_of_the_file" // nom du fichier à modifier
 #define FTP_DIRNAME "folder_on_FTP" // répertoire du fichier dans le FTP
